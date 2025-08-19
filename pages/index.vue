f152cd1ba2583d0a0095b982e595b5ca0a5a2cc8
PipeVerach <felipe@kuantica.cl>
2025-08-19 00:14:57 -0400

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="w-full h-screen flex justify-center items-center bg-[#020420]" >
+    <h1 class="text-white text-4xl" >Welcome Nuxt 4.0</h1>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
